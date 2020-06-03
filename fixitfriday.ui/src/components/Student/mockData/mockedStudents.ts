bda889b1cfda65c038909c91c48ee33d55b2d77f
Kurt Johnson <kurt.johnson@headspring.com>
2020-06-03 17:34:32 -0500

    Create mocked students data
    Signed-off-by: Kurt Johnson <kurt.johnson@headspring.com>

---
+++ fixitfriday.ui/src/components/Student/mockData/mockedStudents.ts
@@ -0,0 +1,276 @@
+import { StudentDetailType } from '../types/StudentDetailTypes';
+
+const allStudents: Array<StudentDetailType> = [
+  {
+    id: '1',
+    firstName: 'John',
+    lastName: 'Doe',
+    middleName: '1',
+    pictureurl: '',
+    email: '',
+    guardians: [{
+      id: '1',
+      firstName: 'Mother',
+      lastName: 'Test',
+      phone: '512-555-1212',
+      address: '124 Main St, Austin, TX, USA 78705',
+    },{
+      id: '2',
+      firstName: 'Father',
+      lastName: 'Test',
+      phone: '512-555-3434',
+      address: '123 Main St, Austin, TX, USA 78705',
+    }, {
+      id: '3',
+      firstName: 'Grandma',
+      lastName: 'Test',
+      phone: '512-555-5656',
+      address: '498 Oak Dr, Austin, TX, USA 78745',
+    }]
+  },
+  {
+    id: '2',
+    firstName: 'John',
+    lastName: 'Doe',
+    middleName: '2',
+    pictureurl: '',
+    email: 'jdoe@test.com',
+    guardians: [{
+  id: '1',
+  firstName: 'Mother',
+  lastName: 'Test',
+  phone: '512-555-1212',
+  address: '124 Main St, Austin, TX, USA 78705',
+},{
+  id: '2',
+  firstName: 'Father',
+  lastName: 'Test',
+  phone: '512-555-3434',
+  address: '123 Main St, Austin, TX, USA 78705',
+}, {
+  id: '3',
+  firstName: 'Grandma',
+  lastName: 'Test',
+  phone: '512-555-5656',
+  address: '498 Oak Dr, Austin, TX, USA 78745',
+}]
+  },
+  {
+    id: '3',
+    firstName: 'John',
+    lastName: 'Doe',
+    middleName: '3',
+    pictureurl: '',
+    email: 'jdoe@test.com',
+    guardians: [{
+      id: '1',
+      firstName: 'Mother',
+      lastName: 'Test',
+      phone: '512-555-1212',
+      address: '124 Main St, Austin, TX, USA 78705',
+    },{
+      id: '2',
+      firstName: 'Father',
+      lastName: 'Test',
+      phone: '512-555-3434',
+      address: '123 Main St, Austin, TX, USA 78705',
+    }, {
+      id: '3',
+      firstName: 'Grandma',
+      lastName: 'Test',
+      phone: '512-555-5656',
+      address: '498 Oak Dr, Austin, TX, USA 78745',
+    }]
+  },
+  {
+    id: '4',
+    firstName: 'John',
+    lastName: 'Doe',
+    middleName: '4',
+    pictureurl: '',
+    email: 'jdoe@test.com',
+    guardians: [{
+      id: '1',
+      firstName: 'Mother',
+      lastName: 'Test',
+      phone: '512-555-1212',
+      address: '124 Main St, Austin, TX, USA 78705',
+    },{
+      id: '2',
+      firstName: 'Father',
+      lastName: 'Test',
+      phone: '512-555-3434',
+      address: '123 Main St, Austin, TX, USA 78705',
+    }, {
+      id: '3',
+      firstName: 'Grandma',
+      lastName: 'Test',
+      phone: '512-555-5656',
+      address: '498 Oak Dr, Austin, TX, USA 78745',
+    }]
+  },
+  {
+    id: '5',
+    firstName: 'John',
+    lastName: 'Doe',
+    middleName: '5',
+    pictureurl: '',
+    email: 'jdoe@test.com',
+    guardians: [{
+      id: '1',
+      firstName: 'Mother',
+      lastName: 'Test',
+      phone: '512-555-1212',
+      address: '124 Main St, Austin, TX, USA 78705',
+    },{
+      id: '2',
+      firstName: 'Father',
+      lastName: 'Test',
+      phone: '512-555-3434',
+      address: '123 Main St, Austin, TX, USA 78705',
+    }, {
+      id: '3',
+      firstName: 'Grandma',
+      lastName: 'Test',
+      phone: '512-555-5656',
+      address: '498 Oak Dr, Austin, TX, USA 78745',
+    }]
+  },
+  {
+    id: '6',
+    firstName: 'John',
+    lastName: 'Doe',
+    middleName: '6',
+    pictureurl: '',
+    email: 'jdoe@test.com',
+    guardians: [{
+      id: '1',
+      firstName: 'Mother',
+      lastName: 'Test',
+      phone: '512-555-1212',
+      address: '124 Main St, Austin, TX, USA 78705',
+    },{
+      id: '2',
+      firstName: 'Father',
+      lastName: 'Test',
+      phone: '512-555-3434',
+      address: '123 Main St, Austin, TX, USA 78705',
+    }, {
+      id: '3',
+      firstName: 'Grandma',
+      lastName: 'Test',
+      phone: '512-555-5656',
+      address: '498 Oak Dr, Austin, TX, USA 78745',
+    }]
+  },
+  {
+    id: '7',
+    firstName: 'John',
+    lastName: 'Doe',
+    middleName: '7',
+    pictureurl: '',
+    email: 'jdoe@test.com',
+    guardians: [{
+      id: '1',
+      firstName: 'Mother',
+      lastName: 'Test',
+      phone: '512-555-1212',
+      address: '124 Main St, Austin, TX, USA 78705',
+    },{
+      id: '2',
+      firstName: 'Father',
+      lastName: 'Test',
+      phone: '512-555-3434',
+      address: '123 Main St, Austin, TX, USA 78705',
+    }, {
+      id: '3',
+      firstName: 'Grandma',
+      lastName: 'Test',
+      phone: '512-555-5656',
+      address: '498 Oak Dr, Austin, TX, USA 78745',
+    }]
+  },
+  {
+    id: '8',
+    firstName: 'John',
+    lastName: 'Doe',
+    middleName: '8',
+    pictureurl: '',
+    email: 'jdoe@test.com',
+    guardians: [{
+      id: '1',
+      firstName: 'Mother',
+      lastName: 'Test',
+      phone: '512-555-1212',
+      address: '124 Main St, Austin, TX, USA 78705',
+    },{
+      id: '2',
+      firstName: 'Father',
+      lastName: 'Test',
+      phone: '512-555-3434',
+      address: '123 Main St, Austin, TX, USA 78705',
+    }, {
+      id: '3',
+      firstName: 'Grandma',
+      lastName: 'Test',
+      phone: '512-555-5656',
+      address: '498 Oak Dr, Austin, TX, USA 78745',
+    }]
+  },
+  {
+    id: '9',
+    firstName: 'John',
+    lastName: 'Doe',
+    middleName: '9',
+    pictureurl: '',
+    email: 'jdoe@test.com',
+    guardians: [{
+      id: '1',
+      firstName: 'Mother',
+      lastName: 'Test',
+      phone: '512-555-1212',
+      address: '124 Main St, Austin, TX, USA 78705',
+    },{
+      id: '2',
+      firstName: 'Father',
+      lastName: 'Test',
+      phone: '512-555-3434',
+      address: '123 Main St, Austin, TX, USA 78705',
+    }, {
+      id: '3',
+      firstName: 'Grandma',
+      lastName: 'Test',
+      phone: '512-555-5656',
+      address: '498 Oak Dr, Austin, TX, USA 78745',
+    }]
+  },
+  {
+    id: '10',
+    firstName: 'John',
+    lastName: 'Doe',
+    middleName: '10',
+    pictureurl: '',
+    email: 'jdoe@test.com',
+    guardians: [{
+      id: '1',
+      firstName: 'Mother',
+      lastName: 'Test',
+      phone: '512-555-1212',
+      address: '124 Main St, Austin, TX, USA 78705',
+    },{
+      id: '2',
+      firstName: 'Father',
+      lastName: 'Test',
+      phone: '512-555-3434',
+      address: '123 Main St, Austin, TX, USA 78705',
+    }, {
+      id: '3',
+      firstName: 'Grandma',
+      lastName: 'Test',
+      phone: '512-555-5656',
+      address: '498 Oak Dr, Austin, TX, USA 78745',
+    }]
+  },
+];
+
+export default allStudents;
