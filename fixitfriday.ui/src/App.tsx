32fcabdb823d3714e7ce0dc8e6acb4c5c89f12c5
Kurt Johnson <kurt.johnson@headspring.com>
2020-06-03 16:30:05 -0500

    Add React Router
    Signed-off-by: Kurt Johnson <kurt.johnson@headspring.com>

--- fixitfriday.ui/src/App.tsx
+++ fixitfriday.ui/src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './app.css';
 import TeacherScreen from './components/teacher-screen/teacherScreen';
+import StudentDetail from './components/Student/StudentDetail';
 
 const App = () => (
   <Container>
-    <TeacherScreen />
+    <Router>
+      <Switch>
+        <Route path="/" exact component={TeacherScreen} />
+        <Route path="/student/:id" exact component={StudentDetail} />
+      </Switch>
+    </Router>
   </Container>
 );
 
