c226596a517db401fc49816723253757801ae837
Kurt Johnson <kurt.johnson@headspring.com>
2020-06-04 14:12:58 -0500

    Add student surveys
    Signed-off-by: Kurt Johnson <kurt.johnson@headspring.com>

--- fixitfriday.ui/src/components/Student/types/StudentDetailTypes.ts
+++ fixitfriday.ui/src/components/Student/types/StudentDetailTypes.ts
@@ -1,5 +1,17 @@
 import { StudentDetailGuardianType } from "./StudentDetailGuardianType";
 
+export type SurveyQuestionType = {
+  id: string,
+  question: string,
+  answer: string,
+}
+
+export type StudentDetailSurveyType = {
+  id: string,
+  name: string,
+  questions: Array<SurveyQuestionType>;
+};
+
 export type StudentDetailType = {
   id: string;
   firstName: string;
@@ -8,4 +20,6 @@ export type StudentDetailType = {
   pictureurl: string;
   email: string;
   guardians: Array<StudentDetailGuardianType>;
+  surveys?: Array<StudentDetailSurveyType>;
 };
+
