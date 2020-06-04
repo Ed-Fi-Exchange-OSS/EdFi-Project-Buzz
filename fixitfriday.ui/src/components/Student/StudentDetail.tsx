9d719dcda1f3dc13de3b04a507fe274f5864693a
Kurt Johnson <kurt.johnson@headspring.com>
2020-06-04 13:35:24 -0500

    Fix layout cards for student detail
    Signed-off-by: Kurt Johnson <kurt.johnson@headspring.com>

--- fixitfriday.ui/src/components/Student/StudentDetail.tsx
+++ fixitfriday.ui/src/components/Student/StudentDetail.tsx
@@ -5,9 +5,29 @@ import StudentGuardianContainer from './StudentGuardianContainer';
 import allStudents from './mockData/mockedStudents';
 import ErrorMessage from '../utilities/ErrorMessage';
 import { StudentDetailProps } from './StudentDetailProps';
+import { Container, Row, Col, CardDeck, Card } from 'react-bootstrap';
+import ProfilePic from '../utilities/ProfilePic';
 
 const StudentDetail: FunctionComponent<StudentDetailProps> = ({ match }) => {
   const [student, setStudent] = useState<StudentDetailType | undefined>(undefined);
+
+  let header = {
+    padding: '25px 10px',
+  };
+
+  let studentContainer = {
+
+  };
+
+  let studentCard = {
+    padding: '10px 10px',
+    margin: '12px 12px',
+  };
+
+  let surveyStyle = {
+    padding: '25px 12px',
+
+  };
 
   useEffect(() => {
     const ourStudent = allStudents.filter((s) => s.id === match.params.id)[0];
@@ -15,24 +35,63 @@ const StudentDetail: FunctionComponent<StudentDetailProps> = ({ match }) => {
   }, [match.params.id]);
 
   return student && student !== undefined ? (
-    <div className="studentDetailContainer">
-      <div className="studentDetailHeader">{`Student Detail - ${student.firstName} ${student.lastName}`}</div>
-      <div className="studentDetailEmail">{`${student.email}`}</div>
-      <div className="studentDetailId">{`${student.id}`}</div>
-      <div id="guardians" className="studentDetailGuardiansContainer">
-        {student.guardians.map((value: StudentDetailGuardianType) => (
-          <StudentGuardianContainer
-            key={value.id}
-            id={value.id}
-            firstName={value.firstName}
-            lastName={value.lastName}
-            phone={value.phone}
-            address={value.address}
-          />
-        ))}
-        ))
-      </div>
-    </div>
+    <Container fluid>
+      <Row style={header}>
+        <Col>
+          <h1>{`Student Detail - ${student.firstName} ${student.lastName}`}</h1>
+          <hr />
+        </Col>
+      </Row>
+      <Row>
+        <Col sm={4} style={studentContainer}>
+          <Card
+            key={student.id}
+            style={{
+              flex: '1',
+              border: '1px solid #696969',
+              minWidth: '21rem',
+              maxWidth: '21rem',
+              padding: '5px 5px',
+              margin: '10px 10px',
+            }}
+          >
+            <Card.Body style={{ display: 'flex' }}>
+              <div
+                style={{
+                  flex: 1,
+                  margin: '2px 2em 2px 2px',
+                  minWidth: '70px',
+                }}
+              >
+                <ProfilePic pictureUrl={student.pictureurl} />
+              </div>
+              <div style={{ flex: 4 }}>
+                <div>{student.email}</div>
+                <div>Student ID: {student.id}</div>
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <CardDeck>
+            {student.guardians.map((value: StudentDetailGuardianType) => (
+              <StudentGuardianContainer
+                key={value.id}
+                id={value.id}
+                firstName={value.firstName}
+                lastName={value.lastName}
+                phone={value.phone}
+                address={value.address}
+                pictureurl={value.pictureurl}
+              />
+            ))}
+          </CardDeck>
+        </Col>
+      </Row>
+      <Row style={surveyStyle}>
+        <Col>THIS IS TEXT FOR THE SURVEY</Col>
+      </Row>
+    </Container>
   ) : (
     <ErrorMessage message={`There is no student with an ID of '${match.params.id}'`} />
   );
--- fixitfriday.ui/src/components/Student/StudentGuardianContainer.tsx
+++ fixitfriday.ui/src/components/Student/StudentGuardianContainer.tsx
@@ -1,13 +1,46 @@
 import React, { FunctionComponent } from 'react';
 import { StudentDetailGuardianType } from './types/StudentDetailGuardianType';
+import { Card, Col } from 'react-bootstrap';
+import ProfilePic from '../utilities/ProfilePic';
 
-const StudentGuardianContainer: FunctionComponent<StudentDetailGuardianType> = ({ id, firstName, lastName, phone, address }) => {
+const StudentGuardianContainer: FunctionComponent<StudentDetailGuardianType> = ({
+  id,
+  firstName,
+  lastName,
+  phone,
+  address,
+  pictureurl,
+}) => {
   return (
-    <div className={"studentGuardian"} >
-      <div>{`${firstName} ${lastName}`}</div>
-      <div>{`${phone}`}</div>
-      <div>{`${address}`}</div>
-    </div>
+    <Card
+      key={id}
+      style={{
+        flex: '1',
+        border: '1px solid #696969',
+        minWidth: '18rem',
+        maxWidth: '18rem',
+        padding: '5px 5px',
+        margin: '10px 10px',
+      }}
+    >
+      <Card.Body style={{ display: 'flex' }}>
+        <div style={{ flex: 3 }}>
+          <Card.Title>{`${firstName} ${lastName}`}</Card.Title>
+          <Card.Subtitle>Relationship</Card.Subtitle>
+          <Card.Text>{`${phone}`}</Card.Text>
+          <Card.Text>{`${address}`}</Card.Text>
+        </div>
+        <div
+          style={{
+            flex: 1,
+            margin: '2px 2em 2px 2px',
+            minWidth: '70px',
+          }}
+        >
+          <ProfilePic pictureUrl={pictureurl} />
+        </div>
+      </Card.Body>
+    </Card>
   );
 };
 
