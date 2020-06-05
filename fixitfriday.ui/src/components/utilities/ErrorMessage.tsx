0dbd5953084882884dc806a0511788741e747f34
Kurt Johnson <kurt.johnson@headspring.com>
2020-06-05 08:23:21 -0500

    Fix error message component
    It should show the error message when there is one
    
    Signed-off-by: Kurt Johnson <kurt.johnson@headspring.com>

--- fixitfriday.ui/src/components/utilities/ErrorMessage.tsx
+++ fixitfriday.ui/src/components/utilities/ErrorMessage.tsx
@@ -7,7 +7,7 @@ type ErrorMessageProps = {
 
 const ErrorMessage: FunctionComponent<ErrorMessageProps> = ({ message }) => {
   return (
-    <Alert variant="danger">{message && message !== null ? 'An error has occurred processing the request.' : message}</Alert>
+    <Alert variant="danger">{message && message !== null ? message : 'An error has occurred processing the request.'}</Alert>
   );
 };
 
