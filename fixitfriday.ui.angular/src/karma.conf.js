312398ac33593fd6eaede684762cbcf3bdba3dbb
Stephen A. Fuqua <stephen@safnet.com>
2020-07-02 10:42:52 -0500

    Don't fail on no tests

--- fixitfriday.ui.angular/src/karma.conf.js
+++ fixitfriday.ui.angular/src/karma.conf.js
@@ -27,6 +27,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome', 'ChromeHeadless'],
-    singleRun: false
+    singleRun: false,
+    failOnEmptyTestSuite: false
   });
 };
