42cd1a9a29d85d60dcee81e6bffdf1f312c19d68
Kurt Johnson <kurt.johnson@headspring.com>
2020-07-23 15:41:19 -0500

    Add survey answers only for staff's students
    When we load data, skip those who are not students for the loading staffkey

--- EdFi.Buzz.Etl/processors/surveyProcessor.js
+++ EdFi.Buzz.Etl/processors/surveyProcessor.js
@@ -14,23 +14,28 @@ const { pgConfig } = require('../config/dbs');
 
 const SURVEY_DATE_FIELD = 'Timestamp';
 const STUDENT_SCHOOL_KEY_FIELD = 'StudentUniqueId';
-const METADATA_FIELDS = [SURVEY_DATE_FIELD, STUDENT_SCHOOL_KEY_FIELD, 'FirstName', 'LastSurname', 'ElectronicMailAddress', 'GradeLevel', 'StudentsELATeacher'];
+const METADATA_FIELDS = [
+  SURVEY_DATE_FIELD,
+  STUDENT_SCHOOL_KEY_FIELD,
+  'FirstName',
+  'LastSurname',
+  'ElectronicMailAddress',
+  'GradeLevel',
+  'StudentsELATeacher',
+];
 
 async function getDB() {
   const connectionString = `postgres://${pgConfig.user}:${pgConfig.password}@${pgConfig.host}:${pgConfig.port}/${pgConfig.database}`;
 
   const client = new Client({ connectionString });
-  await client.connect()
-    .catch((e) => console.error(e));
+  await client.connect().catch((e) => console.error(e));
   return client;
 }
 
 async function isAdminSurveyLoader(staffkey, db) {
   return db
     .query('SELECT isadminsurveyloader FROM buzz.staff where staffkey = $1;', [staffkey])
-    .then(async (result) => {
-      return Boolean(result.rows[0]);
-    });
+    .then(async (result) => Boolean(result.rows[0]));
 }
 
 async function Extract(fileName) {
@@ -39,7 +44,9 @@ async function Extract(fileName) {
   return new Promise((resolve) => {
     fs.createReadStream(fileName)
       .pipe(csv())
-      .on('headers', (headers) => { questions = headers; })
+      .on('headers', (headers) => {
+        questions = headers;
+      })
       .on('data', (row) => answers.push(row))
       .on('end', () => {
         resolve({ questions, answers });
@@ -62,46 +69,62 @@ async function getOrSaveSurvey(surveytitle, db) {
 }
 
 async function getQuestions(surveykey, db) {
-  return db.query('SELECT * FROM buzz.surveyquestion sq where sq.surveykey = $1', [surveykey])
-    .then((result) => result.rows);
+  return db.query('SELECT * FROM buzz.surveyquestion sq where sq.surveykey = $1', [surveykey]).then((result) => result.rows);
 }
 
 async function getOrSaveQuestions(questions, surveykey, db) {
   const dbQuestions = await getQuestions(surveykey, db);
-  const toSave = questions
-    .filter((h) => !dbQuestions.some((q) => q.question.toUpperCase() === h.toUpperCase())
-      && !METADATA_FIELDS.some((f) => f.toUpperCase() === h.toUpperCase()));
+  const toSave = questions.filter(
+    (h) => !dbQuestions.some((q) => q.question.toUpperCase() === h.toUpperCase())
+      && !METADATA_FIELDS.some((f) => f.toUpperCase() === h.toUpperCase()),
+  );
 
   const insertPromises = [];
 
   for (let i = 0; i < toSave.length; i += 1) {
-    insertPromises.push(db.query('INSERT INTO buzz.surveyquestion (surveykey, question) VALUES($1, $2);', [surveykey, toSave[i]]));
+    insertPromises.push(
+      db.query('INSERT INTO buzz.surveyquestion (surveykey, question) VALUES($1, $2);', [surveykey, toSave[i]]),
+    );
   }
   await Promise.allSettled(insertPromises);
 
-  return db.query('SELECT * FROM buzz.surveyquestion sq where sq.surveykey = $1', [surveykey])
-    .then((result) => result.rows);
+  return db.query('SELECT * FROM buzz.surveyquestion sq where sq.surveykey = $1', [surveykey]).then((result) => result.rows);
 }
 
-async function getOrSaveStudentSurvey(surveykey, studentAnswers, db) {
-  const date = studentAnswers[SURVEY_DATE_FIELD];
+async function getOrSaveStudentSurvey(staffkey, surveykey, studentAnswers, db) {
+  const surveyDateField = studentAnswers[SURVEY_DATE_FIELD];
   const studentkey = studentAnswers[STUDENT_SCHOOL_KEY_FIELD];
-  const studentSchoolKeyRow = await db.query('SELECT studentschoolkey FROM buzz.studentschool s WHERE studentkey = $1', [studentkey]);
+
+  const studentSchoolKeyRow = await db.query(
+    'SELECT DISTINCT s.studentschoolkey FROM buzz.studentschool s INNER JOIN buzz.studentsection ss ON s.studentschoolkey = ss.studentschoolkey INNER JOIN buzz.staffsectionassociation ssa ON ss.sectionkey = ssa.sectionkey WHERE s.studentkey = $1 AND ssa.staffkey = $2',
+    [studentkey, staffkey],
+  );
   if (studentSchoolKeyRow.rows.length === 0) {
-    console.error(`ERROR: StudentUniqueId (${studentkey}) not found `);
+    console.error(`ERROR: StudentUniqueId (${studentkey}) not found as a valid student for staff (${staffkey}) `);
     return null;
   }
+
   const { studentschoolkey } = studentSchoolKeyRow.rows[0];
 
-  const result = await db
-    .query('SELECT studentsurveykey, surveykey, studentschoolkey, "date" FROM buzz.studentsurvey where surveykey = $1 and studentschoolkey = $2; ', [surveykey, studentschoolkey]);
-
-  if (result.rows.length > 0) { return result.rows[0]; }
+  const result = await db.query(
+    'SELECT studentsurveykey, surveykey, studentschoolkey, "date" FROM buzz.studentsurvey where surveykey = $1 and studentschoolkey = $2; ',
+    [surveykey, studentschoolkey],
+  );
+  if (result.rows.length > 0) {
+    return result.rows[0];
+  }
 
   return db
-    .query('INSERT INTO buzz.studentsurvey (surveykey, studentschoolkey, "date") VALUES($1, $2, $3) RETURNING *;', [surveykey, studentschoolkey, date])
+    .query('INSERT INTO buzz.studentsurvey (surveykey, studentschoolkey, "date") VALUES($1, $2, $3) RETURNING *;', [
+      surveykey,
+      studentschoolkey,
+      surveyDateField,
+    ])
     .then((resultInsert) => resultInsert.rows[0])
-    .catch((err) => { console.error(`ERROR: ${err.detail}`); return null; });
+    .catch((err) => {
+      console.error(`ERROR: ${err.detail}`);
+      return null;
+    });
 }
 
 async function saveStudentAnswers(studentsurvey, questionKeyMap, currentAnswer, db, surveyProfile) {
@@ -114,26 +137,30 @@ async function saveStudentAnswers(studentsurvey, questionKeyMap, currentAnswer, 
   for (let j = 0; j < currentAnswerKeys.length; j += 1) {
     const q = currentAnswerKeys[j];
     if (questionKeyMap[q]) {
-      promises.push(db
-        .query(`
+      promises.push(
+        db
+          .query(
+            `
 INSERT INTO buzz.studentsurveyanswer (studentsurveykey, surveyquestionkey, answer)
 SELECT $1, $2, $3
 WHERE NOT EXISTS (SELECT FROM buzz.studentsurveyanswer WHERE studentsurveykey = $1 AND surveyquestionkey = $2)
-`, [studentsurvey.studentsurveykey, questionKeyMap[q], currentAnswer[q]])
-        .then((result) => {
-          if (result.rowCount > 0) {
-            surveyProfile.answers.load += 1;
-          } else {
-            surveyProfile.answers.alreadyLoaded += 1;
-          }
-        }));
+`,
+            [studentsurvey.studentsurveykey, questionKeyMap[q], currentAnswer[q]],
+          )
+          .then((result) => {
+            if (result.rowCount > 0) {
+              surveyProfile.answers.load += 1;
+            } else {
+              surveyProfile.answers.alreadyLoaded += 1;
+            }
+          }),
+      );
     }
   }
   return Promise.allSettled(promises);
 }
 
-async function saveAllStudentsAnswers(staffkey, surveykey, questions,
-  studentSurveyAnswers, db, surveyProfile) {
+async function saveAllStudentsAnswers(staffkey, surveykey, questions, studentSurveyAnswers, db, surveyProfile) {
   const questionKeyMap = {};
   questions.forEach((element) => {
     questionKeyMap[element.question] = element.surveyquestionkey;
@@ -141,9 +168,9 @@ async function saveAllStudentsAnswers(staffkey, surveykey, questions,
   const promises = [];
   for (let i = 0; i < studentSurveyAnswers.length; i += 1) {
     const currentAnswer = studentSurveyAnswers[i];
-    promises.push(getOrSaveStudentSurvey(staffkey, surveykey, currentAnswer, db)
-      .then((studentsurvey) => saveStudentAnswers(studentsurvey, questionKeyMap,
-        currentAnswer, db, surveyProfile)));
+    promises.push(
+      getOrSaveStudentSurvey(staffkey, surveykey, currentAnswer, db).then((studentsurvey) => saveStudentAnswers(studentsurvey, questionKeyMap, currentAnswer, db, surveyProfile)),
+    );
   }
   return Promise.allSettled(promises);
 }
@@ -159,9 +186,7 @@ async function Load(staffkey, surveytitle, questions, answers, db) {
     },
   };
   survey.questions = await getOrSaveQuestions(questions, survey.surveykey, db);
-  await saveAllStudentsAnswers(
-    staffkey, survey.surveykey, survey.questions, answers, db, surveyProfile,
-  );
+  await saveAllStudentsAnswers(staffkey, survey.surveykey, survey.questions, answers, db, surveyProfile);
   await db.end();
   return surveyProfile;
 }
