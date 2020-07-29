adc23c696efee51d3afffb0329f9e8319f9ae159
Kurt Johnson <kurt.johnson@headspring.com>
2020-07-29 15:41:40 -0500

    Apply Xavier's updates to survey service test
    Xavier added EnvironmentService and Apollo to the spec to get it to pass.

--- EdFi.Buzz.UI.Angular/src/app/Services/survey.service.spec.ts
+++ EdFi.Buzz.UI.Angular/src/app/Services/survey.service.spec.ts
@@ -1,10 +1,17 @@
 import { TestBed } from '@angular/core/testing';
-
 import { SurveyService } from './survey.service';
-
+import { Apollo } from 'apollo-angular';
+import { EnvironmentService } from './environment.service';
 describe('SurveyService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
-
+  /* Needed for EnvironmentService */
+  window['tempConfigStorage'] = {
+    GQL_ENDPOINT: "Xtest",
+    GOOGLE_CLIENT_ID: "Xtest",
+    SURVEY_MAX_FILE_SIZE_BYTES: 10
+  }
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [Apollo, EnvironmentService]
+  }));
   it('should be created', () => {
     const service: SurveyService = TestBed.get(SurveyService);
     expect(service).toBeTruthy();
