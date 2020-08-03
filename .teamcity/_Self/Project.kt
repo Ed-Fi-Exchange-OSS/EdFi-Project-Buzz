7af8583cc6c3815fa61c8adc9b23a14e84c73335
Kurt Johnson <kurt.johnson@headspring.com>
2020-08-03 12:47:37 -0500

    Add ETLProject to Kotlin to create subproject

--- .teamcity/_Self/Project.kt
+++ .teamcity/_Self/Project.kt
@@ -23,6 +23,7 @@ object BuzzProject : Project({
     }
 
     subProject(ui.UIProject)
+    subProject(etl.ETLProject)
     subProject(api.APIProject)
     subProject(angular.AngularProject)
     subProject(database.DatabaseProject)
