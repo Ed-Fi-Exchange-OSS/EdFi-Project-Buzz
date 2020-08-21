4521ffb435cd10871c8a592b1cbd9dca7d4116e6
Kurt Johnson <kurt.johnson@headspring.com>
2020-08-21 16:09:22 -0500

    Randomize the sample surveys
    The sample surveys did not have enough answers

--- EdFi.Buzz.Etl/surveySampleData/Create-Contact-Survey-Data.sql
+++ EdFi.Buzz.Etl/surveySampleData/Create-Contact-Survey-Data.sql
@@ -1,7 +1,29 @@
 -- CREATE A SAMPLE CONTACT SURVEY
-SELECT DISTINCT NOW() AS "Timestamp", s.studentkey AS "StudentUniqueId", s.studentfirstname AS "FirstName", s.studentlastname AS "LastSurname", 'Phone' AS "Preferred contact method", 'After 6pm' AS "Best time to contact", 'Speak loud' AS "Contact notes"
+CREATE TEMPORARY TABLE IF NOT EXISTS tmp_contact_survey(
+"timestamp" VARCHAR(255),
+StudentUniqueId VARCHAR(255),
+FirstName VARCHAR(255),
+LastSurName VARCHAR(255),
+"Preferred contact method" VARCHAR(255),
+"Best time to contact" VARCHAR(255),
+"Contact notes" VARCHAR(255),
+PRIMARY KEY (StudentUniqueId)
+);
+
+DELETE FROM tmp_contact_survey;
+
+INSERT INTO tmp_contact_survey (StudentUniqueId, FirstName, LastSurname)
+SELECT DISTINCT s.studentschoolkey AS "StudentUniqueId", s.studentfirstname AS "FirstName", s.studentlastname AS "LastSurname"
 FROM buzz.studentschool s
 INNER JOIN buzz.studentsection ss ON s.studentschoolkey = ss.studentschoolkey
 INNER JOIN buzz.staffsectionassociation ssa ON ss.sectionkey = ssa.sectionkey
-WHERE
-ssa.staffkey = 1030
+WHERE ssa.staffkey = 1030;
+
+UPDATE tmp_contact_survey
+SET
+"timestamp" = NOW()
+, "Preferred contact method" = CASE floor(RANDOM() * (3-1+1) + 1)::INT WHEN 1 THEN 'SMS' WHEN 2 THEN 'Phone' WHEN 3 THEN 'Email' END
+, "Best time to contact" = CASE floor(RANDOM() * (4-1+1) + 1)::INT WHEN 1 THEN 'After work hours' WHEN 2 THEN 'During school hours' WHEN 3 THEN 'After dinner' WHEN 4 THEN 'Weeknights between 6PM and 9PM' WHEN 4 THEN 'Mornings after 9am' END
+, "Contact notes" = CASE floor(RANDOM() * (4-1+1) + 1)::INT WHEN 1 THEN 'Speak loudly' WHEN 2 THEN 'Works nights' WHEN 3 THEN 'Actually prefers email sometimes' WHEN 4 THEN 'Phone has issues' WHEN 4 THEN 'Speak slowly' END;
+
+SELECT * FROM tmp_contact_survey ORDER BY 2;
--- EdFi.Buzz.Etl/surveySampleData/Create-Sample-Internet-Access-Survey-Data.sql
+++ EdFi.Buzz.Etl/surveySampleData/Create-Sample-Internet-Access-Survey-Data.sql
@@ -1,7 +1,31 @@
 -- CREATE A SAMPLE INTERNET ACCESS SURVEY
-SELECT DISTINCT NOW() AS "Timestamp", s.studentkey AS "StudentUniqueId", s.studentfirstname AS "FirstName", s.studentlastname AS "LastSurname", 'Phone' AS "Preferred contact method", 'After 6pm' AS "Best time to contact", 'Speak loud' AS "Contact notes"
+CREATE TEMPORARY TABLE IF NOT EXISTS tmp_internet_survey(
+"timestamp" VARCHAR(255),
+StudentUniqueId VARCHAR(255),
+FirstName VARCHAR(255),
+LastSurName VARCHAR(255),
+"Internet Access Type" VARCHAR(255),
+"Has School Email Account" VARCHAR(255),
+"Has School Student Account" VARCHAR(255),
+PRIMARY KEY (StudentUniqueId)
+);
+
+DELETE FROM tmp_internet_survey;
+
+INSERT INTO tmp_internet_survey (StudentUniqueId, FirstName, LastSurname) 
+SELECT DISTINCT s.studentkey AS "StudentUniqueId", s.studentfirstname AS "FirstName", s.studentlastname AS "LastSurname"
 FROM buzz.studentschool s
 INNER JOIN buzz.studentsection ss ON s.studentschoolkey = ss.studentschoolkey
 INNER JOIN buzz.staffsectionassociation ssa ON ss.sectionkey = ssa.sectionkey
 WHERE
-ssa.staffkey = 1030
+ssa.staffkey = 1030;
+
+UPDATE tmp_internet_survey 
+SET 
+"timestamp" = NOW(), 
+"Internet Access Type" = CASE floor(RANDOM() * (3-1+1) + 1)::INT WHEN 1 THEN 'High Speed Internet' WHEN 2 THEN 'School provided hot-spot' WHEN 3 THEN 'No Internet' END
+, "Has School Email Account" = CASE floor(RANDOM() * (2-1+1) + 1)::INT WHEN 1 THEN 'Yes' WHEN 2 THEN 'No' END
+, "Has School Student Account" = CASE floor(RANDOM() * (2-1+1) + 1)::INT WHEN 1 THEN 'Yes' WHEN 2 THEN 'No' END;
+
+SELECT "timestamp" AS "Timestamp" , studentuniqueid AS "StudentUniqueId" , firstname AS "FirstName", lastsurname AS "LastSurname", "Internet Access Type" AS "Internet Access Type", "Has School Email Account" AS "Has School Email Account", "Has School Student Account" AS "Has School Student Account"
+FROM tmp_internet_survey ORDER BY 2;
