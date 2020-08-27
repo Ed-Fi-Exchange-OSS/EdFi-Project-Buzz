047d78d0104613792c1449145eca4632e5042bf9
DavidJGapCR <djimenez@growthaccelerationpartners.com>
2020-08-27 10:57:48 -0600

    Final details. Cleaning up the code.

--- EdFi.Buzz.UI.Angular/src/app/AdminSurveyReact/adminSurvey.tsx
+++ EdFi.Buzz.UI.Angular/src/app/AdminSurveyReact/adminSurvey.tsx
@@ -9,7 +9,7 @@ import { FunctionComponent, useState } from 'react';
 import { ApiService } from 'src/app/Services/api.service';
 import { SurveyStatus } from '../Models/survey';
 import styled from 'styled-components';
-import { HeadlineContainer, MainContainer, TitleSpanContainer, TotalRecordsContainer } from '../../buzztheme';
+import { HeadlineContainer, MainContainer, TitleSpanContainer } from '../../buzztheme';
 
 import OrangeSearch from '../../assets/search.png';
 
@@ -140,8 +140,7 @@ export const AdminSurvey: FunctionComponent<AdminSurveyComponentProps> = (props:
       const filteredList = surveyList
         .filter(s => (s.resultSummaryObj.survey.title as string).toUpperCase().includes(upperSearchText));
       setsurveyFilteredList(filteredList);
-    }
-    else {
+    } else {
       setsurveyFilteredList(surveyList);
     }
   }
@@ -226,102 +225,33 @@ export const AdminSurvey: FunctionComponent<AdminSurveyComponentProps> = (props:
                       <a href={`#/app/uploadSurvey/${survey.surveykey}`} className='btn btn-primary ion-md-create'></a>
                     </div>
                   </div>
-                  
+
                 </div>
               </AdminSurveyContainer>
             </div>
             ))}
       </div>
-{/* 
-    <div className='container'>
-      <div className='position-relative p-t-10'>
-        <a className='btn-outline-nav inline-block position-relative' style={{ 'top': '-10px', 'left': '0px'}} href='javascript:history.back(-1)'>
-          <i className='ion ion-md-arrow-dropleft f-s-37 position-absolute' style={{ 'top': '-1px', 'left': '10px' }}></i>
-        </a>
-        <h1 className='inline-block position-absolute' style={{'top': '3px', 'left': '44px'}}>Administrate <span>surveys</span></h1>
-      </div>
 
-      <div className='row'>
-        <div className='col-12'>
-          <div className='card'>
-            <div className='card-body d-flex form-group' style={{'marginBottom': '0'}}>
-              <div className='input-group'>
-                <input onChange={handleChange} ref={surveyFilterRef} type='text' className='form-control' id='searchInputs' placeholder='Search'/>
-                <div className='input-group-append'>
-                  <button className='btn btn-primary' type='button' onClick={handleChange}>
-                    <label>Search</label>
-                      <i className='ion ion-md-search'></i>
-                  </button>
-                </div>
-              </div>
+      {/* Modal */}
+      <div className='modal' id='deletesurveyconfirmation' data-backdrop='static' data-keyboard='false' role='dialog' aria-labelledby='staticBackdropLabel' aria-hidden='true'>
+        <div className='modal-dialog modal-dialog-centered'>
+          <div className='modal-content'>
+            <div className='modal-header'>
+              <h3 className='modal-title' id='deletenoteconfirmationLabel'>Administrate surveys</h3>
+              <button type='button' className='close' data-dismiss='modal' aria-label='Close'>
+                <span aria-hidden='true'>&times;</span>
+              </button>
+            </div>
+            <div className='modal-body'>
+              Are you sure you want to delete this survey?
+            </div>
+            <div className='modal-footer'>
+              <button type='button' className='btn btn-secondary' data-dismiss='modal'>No</button>
+              <button type='button' onClick={deleteSurvey}  data-dismiss='modal' className='btn btn-danger'>Yes</button>
             </div>
           </div>
         </div>
       </div>
-
-      {(!surveyFilteredList) &&
-      <div className='row'>
-        <div className='col-12'>
-          <div className='alert alert-warning'>
-            No survey found.
-          </div>
-        </div>
-      </div>
-      }
-
-      {(surveyFilteredList) &&
-      <div className='row'>
-        {surveyFilteredList.map((survey, index) => {
-          return <div className='col-12 col-md-6' key={survey.surveystatuskey}>
-            <div className='card survey-metadata'>
-                <div className='card-body'>
-                  <div>
-                    <h2>{survey.resultSummaryObj.survey.title}</h2>
-                    <p><span>Survey Key:</span> {survey.surveystatuskey} </p>
-                    <p><span>Job Status:</span> {survey.jobstatus.description} </p>
-                    <p><span>Questions:</span> {survey.resultSummaryObj.survey.questions} </p>
-                    <p><span>Answers Load:</span> {survey.resultSummaryObj.process.load} </p>
-                    <p><span>Answers Rejected:</span> {survey.resultSummaryObj.process.rejected} </p>
-                    <p><span>Answers Already Loaded:</span> {survey.resultSummaryObj.process.alreadyLoaded} </p>
-                  </div>
-                </div>
-                <div className='row'>
-                  <div className='col-2 offset-8'>
-                    <span onClick={(e) => setSurveyToDelete(survey.surveykey)} className='btn btn-danger btn-delete-note ion-md-trash'
-                      data-toggle='modal' data-target='#deletesurveyconfirmation'></span>
-                  </div>
-                  <div className='col-2'>
-                    <a href={`#/app/uploadSurvey/${survey.surveykey}`} className='btn btn-primary ion-md-create'></a>
-                  </div>
-                </div>
-            </div>
-          </div>;
-          })
-        }
-      </div>
-      }
-    </div> */}
-
-    {/* Modal */}
-    <div className='modal' id='deletesurveyconfirmation' data-backdrop='static' data-keyboard='false' role='dialog' aria-labelledby='staticBackdropLabel' aria-hidden='true'>
-      <div className='modal-dialog modal-dialog-centered'>
-        <div className='modal-content'>
-          <div className='modal-header'>
-            <h3 className='modal-title' id='deletenoteconfirmationLabel'>Administrate surveys</h3>
-            <button type='button' className='close' data-dismiss='modal' aria-label='Close'>
-              <span aria-hidden='true'>&times;</span>
-            </button>
-          </div>
-          <div className='modal-body'>
-            Are you sure you want to delete this survey?
-          </div>
-          <div className='modal-footer'>
-            <button type='button' className='btn btn-secondary' data-dismiss='modal'>No</button>
-            <button type='button' onClick={deleteSurvey}  data-dismiss='modal' className='btn btn-danger'>Yes</button>
-          </div>
-        </div>
-      </div>
-    </div>
     </MainContainer>
     </>
   );
