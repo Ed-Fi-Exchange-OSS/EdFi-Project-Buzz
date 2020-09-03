140e3cc47be2fd667f0da6b815ad9145840b3b69
DavidJGapCR <djimenez@growthaccelerationpartners.com>
2020-09-03 14:46:10 -0600

    Fixes linting errors.

--- EdFi.Buzz.Etl/RioRancho/RioRanchoContactsProcessor.js
+++ EdFi.Buzz.Etl/RioRancho/RioRanchoContactsProcessor.js
@@ -13,7 +13,6 @@ const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 const { Client } = require('pg');
 const { pgConfig } = require('../config/dbs');
-const { Console } = require('console');
 
 const studentNumber = 'STUDENTS.Student_Number';
 const contactfirstname = 'STUDENTCOREFIELDS.guardian_fn';
@@ -38,11 +37,11 @@ async function Extract(fileName) {
   return new Promise((resolve, reject) => {
     fs.createReadStream(fileName)
       .on('error', (err) => {
-        reject(err)
+        reject(err);
       })
       .pipe(csv({ separator: '\t' }))
       .on('data', (row) => {
-        data.push(row)
+        data.push(row);
       })
       .on('end', () => {
         resolve({ data });
@@ -51,40 +50,34 @@ async function Extract(fileName) {
 }
 
 async function getStudents(studentKeys, db) {
-  let uniqStudentKeys = [...new Set(studentKeys)];
-  let params = [];
+  const uniqStudentKeys = [...new Set(studentKeys)];
+  const params = [];
 
-  for(var i = 1; i <= uniqStudentKeys.length; i++) {
-    params.push('$' + i);
+  for (let i = 1; i <= uniqStudentKeys.length; i += 1) {
+    params.push(`$${i}`);
   }
 
-  return db.query('SELECT studentkey, studentschoolkey FROM buzz.studentschool WHERE studentkey IN (' + params.join(',') + ')'
-      ,uniqStudentKeys)
-    .then((result) => {
-      return result.rows;
-    })
+  return db.query(`SELECT studentkey, studentschoolkey FROM buzz.studentschool WHERE studentkey IN (${params.join(',')})`,
+    uniqStudentKeys)
+    .then((result) => result.rows)
     .catch((err) => {
       console.error(`ERROR: ${err} - ${err.detail}`);
     });
 }
 
 async function getStudentContacts(students, db) {
-  let studentKeys = students.map(student => {
-    return student.studentkey;
-  });
+  const studentKeys = students.map((student) => student.studentkey);
 
-  let uniqStudentKeys = [...new Set(studentKeys)];
-  let params = [];
+  const uniqStudentKeys = [...new Set(studentKeys)];
+  const params = [];
 
-  for(var i = 1; i <= uniqStudentKeys.length; i++) {
-    params.push('$' + i);
+  for (let i = 1; i <= uniqStudentKeys.length; i += 1) {
+    params.push(`$${i}`);
   }
 
-  return db.query('SELECT uniquekey as contactkey, studentkey, contactfirstname, contactlastname FROM buzz.contactperson WHERE studentkey IN (' + params.join(',') + ')'
-      ,uniqStudentKeys)
-    .then((result) => {
-      return result.rows;
-    })
+  return db.query(`SELECT uniquekey as contactkey, studentkey, contactfirstname, contactlastname FROM buzz.contactperson WHERE studentkey IN (${params.join(',')})`,
+    uniqStudentKeys)
+    .then((result) => result.rows)
     .catch((err) => {
       console.error(`ERROR: ${err} - ${err.detail}`);
     });
@@ -94,99 +87,89 @@ function matchFileContactsWithExistingContactsinDB(fileData, contacts) {
   let newContacts = 0;
   let editContacts = 0;
 
-  fileData.forEach(fileContact => {
-    let contactFound = contacts.find(contact => {
-      return contact.studentkey === fileContact[studentNumber]
-        && contact.contactfirstname === fileContact[contactfirstname]
-        && contact.contactlastname === fileContact[contactlastname]
-    });
+  fileData.forEach((fileCon) => {
+    const contactFound = contacts.find((contact) => contact.studentkey === fileCon[studentNumber]
+        && contact.contactfirstname === fileCon[contactfirstname]
+        && contact.contactlastname === fileCon[contactlastname]);
 
     if (contactFound) {
-      fileContact.contactkey = contactFound.contactkey;
-      editContacts++;
-    }
-    else {
-      fileContact.contactkey = null;
-      newContacts++;
+      fileCon.contactkey = contactFound.contactkey;
+      editContacts += 1;
+    } else {
+      fileCon.contactkey = null;
+      newContacts += 1;
     }
   });
 
   return {
-    newContacts, editContacts
+    newContacts, editContacts,
   };
 }
 
 async function saveContactData(allContactsData, students, db) {
+  const newContactsData = allContactsData.filter((contactData) => contactData.contactkey == null);
 
-  let newContactsData = allContactsData.filter(contactData => {
-    return contactData.contactkey == null;
-  });
-
-  let editContactsData = allContactsData.filter(contactData => {
-    return contactData.contactkey != null;
-  });
+  const editContactsData = allContactsData.filter((contactData) => contactData.contactkey != null);
 
   console.log(`   ...Inserting ${newContactsData.length} contacts`);
 
   const promises = [];
-  newContactsData.forEach(currentContactData => {
-    
-    let newGuid = uuidv4();
+  newContactsData.forEach((currentContact) => {
+    const newGuid = uuidv4();
 
     promises.push(
       db.query(
-          `
+        `
           INSERT INTO buzz.contactperson(uniquekey,contactpersonkey,studentkey,contactfirstname,contactlastname
             ,relationshiptostudent,contactaddress,phonenumber,primaryemailaddress) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
           `,
-          [
-            `${newGuid}-${currentContactData[studentNumber]}`,
-            newGuid, 
-            currentContactData[studentNumber],
-            currentContactData[contactfirstname],
-            currentContactData[contactlastname],
-            'Unknown',
-            `${currentContactData[contactStreet]} ${currentContactData[contactCity]}, ${currentContactData[contactState]} ${currentContactData[contactZip]}`,
-            currentContactData[contactphone],
-            currentContactData[contactemail],
-          ]
-        )
+        [
+          `${newGuid}-${currentContact[studentNumber]}`,
+          newGuid,
+          currentContact[studentNumber],
+          currentContact[contactfirstname],
+          currentContact[contactlastname],
+          'Unknown',
+          `${currentContact[contactStreet]} ${currentContact[contactCity]}, ${currentContact[contactState]} ${currentContact[contactZip]}`,
+          currentContact[contactphone],
+          currentContact[contactemail],
+        ],
+      )
         .then((result) => {
           if (result.rowCount > 0) {
-            console.log(`      ${currentContactData[contactfirstname]} ${currentContactData[contactlastname]} inserted successfully.`);
+            console.log(`      ${currentContact[contactfirstname]} ${currentContact[contactlastname]} inserted successfully.`);
 
-            let studentschoolkey = students.find(student => {
-              return student.studentkey === currentContactData[studentNumber];
-            }).studentschoolkey;
+            // eslint-disable-next-line max-len
+            const { studentschoolkey } = students.find((s) => s.studentkey === currentContact[studentNumber]);
 
             db.query(
               `
               INSERT INTO buzz.studentcontact (contactkey, studentschoolkey) VALUES ($1, $2)
               `,
-              [`${newGuid}-${currentContactData[studentNumber]}`, studentschoolkey]
+              [`${newGuid}-${currentContact[studentNumber]}`, studentschoolkey],
             )
-            .then((result) => {
-              if (result.rowCount > 0) {
-                console.log(`      ${currentContactData[contactfirstname]} ${currentContactData[contactlastname]} associated to student successfully.`);
-              }
-            })
-            .catch((err) => {
-              console.error(`ERROR: ${err} - ${err.detail}`);
-            })
+              .then((resultAssoc) => {
+                if (resultAssoc.rowCount > 0) {
+                  console.log(`      ${currentContact[contactfirstname]} ${currentContact[contactlastname]} associated to student successfully.`);
+                }
+              })
+              .catch((err) => {
+                console.error(`ERROR: ${err} - ${err.detail}`);
+              });
           }
         })
         .catch((err) => {
           console.error(`ERROR: ${err} - ${err.detail}`);
-        })
+        }),
     );
   });
-  
+
   console.log(`   ...Updating ${editContactsData.length} contacts`);
 
-  editContactsData.forEach(currentContactData => {
+  editContactsData.forEach((currentContact) => {
     promises.push(
       db.query(
-          `
+        `
           UPDATE buzz.contactperson SET
             relationshiptostudent = $2,
             contactaddress = $3,
@@ -194,40 +177,38 @@ async function saveContactData(allContactsData, students, db) {
             primaryemailaddress = $5 
           WHERE uniquekey = $1;
           `,
-          [
-            currentContactData.contactkey,
-            'Unknown',
-            `${currentContactData[contactStreet]} ${currentContactData[contactCity]}, ${currentContactData[contactState]} ${currentContactData[contactZip]}`,
-            currentContactData[contactphone],
-            currentContactData[contactemail],
-          ]
-        )
+        [
+          currentContact.contactkey,
+          'Unknown',
+          `${currentContact[contactStreet]} ${currentContact[contactCity]}, ${currentContact[contactState]} ${currentContact[contactZip]}`,
+          currentContact[contactphone],
+          currentContact[contactemail],
+        ],
+      )
         .then((result) => {
           if (result.rowCount > 0) {
-            console.log(`      ${currentContactData[contactfirstname]} ${currentContactData[contactlastname]} updated successfully.`);
+            console.log(`      ${currentContact[contactfirstname]} ${currentContact[contactlastname]} updated successfully.`);
           }
         })
         .catch((err) => {
           console.error(`ERROR: ${err} - ${err.detail}`);
-        })
+        }),
     );
   });
-  
+
   return Promise.allSettled(promises);
 }
 
 const process = async (filename) => {
   const db = await getDB();
-  
+
   console.log('Extract data from the file...');
   const data = await Extract(path.join(filename));
 
   console.log(`   ...${data.data.length} contacts extracted from the file.`);
 
   console.log('Get studentkey and studentschoolkey from database, based on data extracted from the file...');
-  const students = await getStudents(data.data.map(contact => {
-    return contact['STUDENTS.Student_Number'];
-  }), db);
+  const students = await getStudents(data.data.map((contact) => contact['STUDENTS.Student_Number']), db);
 
   console.log(`   ...${students.length} students found in the database.`);
 
@@ -237,7 +218,7 @@ const process = async (filename) => {
   console.log(`   ...${studentContacts.length} contacts found in the database.`);
 
   console.log('Check if the contact already exists. If it does, then add the contactkey to the file contact row...');
-  let match = matchFileContactsWithExistingContactsinDB(data.data, studentContacts);
+  const match = matchFileContactsWithExistingContactsinDB(data.data, studentContacts);
 
   console.log(`   ...Based on previous data collected. We have ${match.newContacts} new contacts to insert, and ${match.editContacts} contacts to update.`);
 
