ca7ba36a34d086351d932bfc20ad8f8ce5fb2cfe
Kurt Johnson <63315476+kurtjohnson-HS@users.noreply.github.com>
2020-09-08 15:58:15 -0500

    [FIF-279] Create Teamcity build for UI (#77)
    * FIF-279 Create and build React app
    
    * Update the path to UI
    
    * Rename UI to UI1 temporarily
    
    * Rename EdFi.Buzz.UI1 back to EdFi.Buzz.UI
    
    * Add eng for deploy and build package script
    
    * Replace Angular's dist output with React's build
    
    Co-authored-by: Xavier Lucatero <xavier@nearshoredevs.com>

--- EdFi.Buzz.UI/src/App.tsx
+++ EdFi.Buzz.UI/src/App.tsx
@@ -10,23 +10,22 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route, useHistory } from 'react-router-dom';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
-import configureDI from './DIContext';
 
-import { ApiService } from './Services/ApiService';
-import { Header } from './Components/Header';
-import { Footer } from './Components/Footer';
-import { Login } from './Feature/Login';
-import { StudentRoster } from './Feature/StudentRoster';
-import { StudentDetail } from './Feature/StudentDetail';
-import { SurveyAnalytics } from './Feature/SurveyAnalytics';
-import { UploadSurvey } from './Feature/UploadSurvey';
-import { AdminSurvey } from './Feature/AdminSurvey';
+import { ApiService } from 'Services/ApiService';
+import { Header } from 'Components/Header';
+import { Footer } from 'Components/Footer';
+import { Login } from 'Feature/Login';
+import { StudentRoster } from 'Feature/StudentRoster';
+import { StudentDetail } from 'Feature/StudentDetail';
+import { SurveyAnalytics } from 'Feature/SurveyAnalytics';
+import { UploadSurvey } from 'Feature/UploadSurvey';
+import { AdminSurvey } from 'Feature/AdminSurvey';
 
 import './App.css';
+import configureDI from 'DIContext';
 
 const container = configureDI();
 const apolloClient = container.get<ApolloClient<InMemoryCache>>("ApolloClient");
-console.log("apolloClient:", apolloClient);
 
 
 export default function App(): JSX.Element {
@@ -35,34 +34,29 @@ export default function App(): JSX.Element {
   return (
     <ApolloProvider client={apolloClient}>
       <Router>
-        <Switch>
-          <Route path="/login" > <Login
-            api={container.get<ApiService>("ApiService")}
-            navigate={(url) => console.log("TODO: implement navigate")}
-            googleClientId="761615059487-5tuhthkic53s5m0e40k6n68hrc7i3udp.apps.googleusercontent.com"
-            returnUrl="/"
+        <div>
+          <Header />
 
-          /> </Route>
-          <Route path="/">
-
-            <div>
-              <Header />
-
-              {/* A <Switch> looks through its children <Route>s and
+          {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-              <Switch>
-                <Route exact path="/"> <StudentRoster /> </Route>
-                <Route path="/studentDetail/:studentKey"> <StudentDetail /> </Route>
-                <Route path="/surveyAnalytics"> <SurveyAnalytics /> </Route>
-                <Route path="/uploadSurvey/:surveyKey"> <UploadSurvey /> </Route>
-                <Route path="/uploadSurvey"> <UploadSurvey /> </Route>
-                <Route path="/adminSurvey"> <AdminSurvey /> </Route>
-              </Switch>
+          <Switch>
+            <Route path="/login"> <Login
+              api={container.get<ApiService>("ApiService")}
+              navigate={(url)=> console.log("TODO: implement navigate")}
+              googleClientId="761615059487-5tuhthkic53s5m0e40k6n68hrc7i3udp.apps.googleusercontent.com"
+              returnUrl="/"
 
-              <Footer />
-            </div>
-          </Route>
-        </Switch>
+            /> </Route>
+            <Route exact path="/"> <StudentRoster /> </Route>
+            <Route path="/studentDetail/:studentKey"> <StudentDetail /> </Route>
+            <Route path="/surveyAnalytics"> <SurveyAnalytics /> </Route>
+            <Route path="/uploadSurvey/:surveyKey"> <UploadSurvey /> </Route>
+            <Route path="/uploadSurvey"> <UploadSurvey /> </Route>
+            <Route path="/adminSurvey"> <AdminSurvey /> </Route>
+          </Switch>
+
+          <Footer />
+        </div>
       </Router>
     </ApolloProvider>
   );
