a2c9bf5e1ddd3a27c505c86e249317a0f3b229c5
Kurt Johnson <kurt.johnson@headspring.com>
2020-09-10 15:52:19 -0500

    Fix App test errors

--- EdFi.Buzz.UI/src/App.test.tsx
+++ EdFi.Buzz.UI/src/App.test.tsx
@@ -5,12 +5,27 @@
  * See the LICENSE and NOTICES files in the project root for more information.
  */
 
-
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+const OLD_ENV = process.env;
+
+beforeEach(() => {
+    jest.resetModules(); // most important - it clears the cache
+    process.env = { ...OLD_ENV }; // make a copy
+});
+
+afterAll(() => {
+    process.env = OLD_ENV; // restore old env
+});
+
+describe('', () => {
+    const OLD_ENV = {};
+
+    beforeEach(() => {});
 
 test('test test', () => {
-  const app = render(<App />);
-  expect(app).toBeDefined();
+        const app = render(<App />);
+        expect(app).toBeDefined();
+    });
 });
