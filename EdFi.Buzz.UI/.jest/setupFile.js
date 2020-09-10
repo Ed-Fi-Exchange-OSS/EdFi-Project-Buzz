e6a64c126d6f7cbb37c3d0e1509a9318e031d59c
Kurt Johnson <kurt.johnson@headspring.com>
2020-09-10 16:33:07 -0500

    Pass string in Environment Service for finish ids

--- EdFi.Buzz.UI/.jest/setupFile.js
+++ EdFi.Buzz.UI/.jest/setupFile.js
@@ -4,9 +4,9 @@
 // See the LICENSE and NOTICES files in the project root for more information.
 
 process.env.PORT = 4200;
-process.env.REACT_APP_GQL_ENDPOINT = "[GQL ENDPOINT]";
-process.env.REACT_APP_GOOGLE_CLIENT_ID = "[GOOGLE CLIENT ID]";
-process.env.xREACT_APP_ADFS_CLIENT_ID = "[ADFS_CLIENT_ID]";
-process.env.REACT_APP_ADFS_TENANT_ID = "[ADFS_TENANT_ID]";
+process.env.REACT_APP_GQL_ENDPOINT = '[GQL ENDPOINT]';
+process.env.REACT_APP_GOOGLE_CLIENT_ID = '[GOOGLE CLIENT ID]';
+process.env.xREACT_APP_ADFS_CLIENT_ID = '[ADFS_CLIENT_ID]';
+process.env.REACT_APP_ADFS_TENANT_ID = '[ADFS_TENANT_ID]';
 process.env.REACT_APP_SURVEY_MAX_FILE_SIZE_BYTES = 1048576;
-process.env.REACT_APP_JOB_STATUS_FINISH_IDS = [3];
+process.env.REACT_APP_JOB_STATUS_FINISH_IDS = '[3]';
