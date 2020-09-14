7be0edac602f01206768a06c3e361b4f249e670c
Kurt Johnson <kurt.johnson@headspring.com>
2020-09-14 15:39:04 -0500

    Create initial folder structure for Buzz Installer

---
+++ .teamcity/Installer/Project.kt
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+package installer
+
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+
+object UIProject : Project({
+    id("Buzz_Installer")
+    name = "Installer"
+    description = "Buzz App Installer"
+
+    buildType(ui.buildTypes.PullRequestUIBuild)
+    buildType(ui.buildTypes.BranchInstallerBuild)
+    buildType(ui.buildTypes.DeployInstallerBuild)
+
+    params{
+        param("project.directory", "./EdFi.Buzz.Installer");
+        param("octopus.release.version","<placeholder value>")
+        param("octopus.release.project", "Project Buzz")
+        param("octopus.project.id", "Projects-112")
+        param("vcs.checkout.rules","""
+            +:.teamcity => .teamcity
+            +:%project.directory% => %project.directory%
+        """.trimIndent())
+    }
+})
