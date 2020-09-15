49c612da26924f478f49084fa4f7f062dd02b73a
jleiva-gap <jleiva@growthaccelerationpartners.com>
2020-09-15 08:44:34 -0600

    Update index.tsx
    Updated to surround the App component with the ErrorBoundary.

--- EdFi.Buzz.UI/src/index.tsx
+++ EdFi.Buzz.UI/src/index.tsx
@@ -8,7 +8,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import ErrorBoundary from './Components/Error/ErrorBoundary';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ErrorBoundary><App /></ErrorBoundary>, document.getElementById('root'));
 
