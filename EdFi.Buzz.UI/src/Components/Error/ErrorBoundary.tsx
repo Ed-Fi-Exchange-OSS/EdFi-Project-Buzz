2fe3f7ec5cb9fd2cce983543ed917a8e47df3484
jleiva-gap <jleiva@growthaccelerationpartners.com>
2020-09-15 12:26:27 -0600

    Fixed ErrorBoundary linting issues

--- EdFi.Buzz.UI/src/Components/Error/ErrorBoundary.tsx
+++ EdFi.Buzz.UI/src/Components/Error/ErrorBoundary.tsx
@@ -1,16 +1,20 @@
-import React, {Component} from 'react';
-import { MainContainer, HeadlineContainer, TitleSpanContainer } from '../../buzztheme';
+import React, {ErrorInfo, ReactNode} from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-interface ErrorBoundaryProps{
+interface ErrorBoundaryState {
+  hasError: boolean;
+  error?: ErrorInfo;
+};
 
-}
+export default class ErrorBoundary extends React.Component  {
+  static propTypes = {
+    children: PropTypes.node.isRequired
+  };
 
-interface ErrorBoundaryState{
-  hasError: boolean
-}
-
-export default class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState>  {
+  public state: ErrorBoundaryState = {
+    hasError: false
+  };
 
   private ErrorMessage = styled.div`
     color: #856404;
@@ -20,34 +24,30 @@ export default class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBo
     border: 3px solid #ffeeba;
     padding: 20px;
   `;
-  constructor(props) {
-       super(props);
-       this.state = {hasError: false };
+
+  static getDerivedStateFromError(error: ErrorInfo): ErrorBoundaryState {    // Update state so the next render will show the fallback UI.
+    return {
+      hasError: true,
+      error
+    };
+  }
+
+  componentDidCatch=(error: Error, info: ErrorInfo): void => {
+    console.error('ErrorBoundary caught an error', error, info);
+  };
+
+  render(): ReactNode {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <><this.ErrorMessage>
+        <h3>Buzz has encountered an unrecoverable error.</h3>
+        <p>Any edits may have been lost.</p>
+        <p>It is best that you refresh your browser, and contact support if the error keeps happening.
+        </p>
+      </this.ErrorMessage></>
+      ;
     }
 
-    static getDerivedStateFromError(error) {    // Update state so the next render will show the fallback UI.
-      return {
-        hasError: true};
-    }
-
-    componentDidCatch=(error, info) => {
-       // You can also log the error to an error reporting service
-
-    }
-
-    render() {
-      if (this.state.hasError)
-      {
-        // You can render any custom fallback UI
-        return <><this.ErrorMessage>
-          <h3>Buzz has encountered an unrecoverable error.</h3>
-          <p>Any edits may have been lost.</p>
-          <p>It is best that you refresh your browser, and contact support if the error keeps happening.
-          </p>
-          </this.ErrorMessage></>
-          ;
-      }
-
-      return this.props.children; ;
-    }
- }
+    return this.props.children; ;
+  }
+}
