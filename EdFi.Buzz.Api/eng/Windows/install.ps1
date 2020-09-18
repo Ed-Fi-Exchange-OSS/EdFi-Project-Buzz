24072a5313145da2628a48da6f19f075e7a45829
Kurt Johnson <kurt.johnson@headspring.com>
2020-09-18 14:42:32 -0500

    Ensure all Buzz apps are installed by admins
    Requires are on all install.ps1 scripts

--- EdFi.Buzz.Api/eng/Windows/install.ps1
+++ EdFi.Buzz.Api/eng/Windows/install.ps1
@@ -3,8 +3,8 @@
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
 
-#requires -version 5
-#requires -RunAsAdministrator
+#Requires -version 5
+#Requires -RunAsAdministrator
 
 [CmdletBinding()]
 param(
