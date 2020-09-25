a516956fd754dac36a34ab892896c07359b8e5c9
NSD-Xavier <64990500+NSD-Xavier@users.noreply.github.com>
2020-09-25 09:12:16 -0500

    [FIF-312] Investigate why the app is throwing an 500 error. And fix if possible. (#103)
    Caused by changes in a platform-@nestjs/platform-fastify@^7.4.4 dependency (fastify "3.3.0")

--- EdFi.Buzz.Api/src/graphql/decorators/currentUser.decorator.ts
+++ EdFi.Buzz.Api/src/graphql/decorators/currentUser.decorator.ts
@@ -7,8 +7,8 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import getClaims from '../services/jwt.service';
 
 const CurrentUser = createParamDecorator((data: unknown, context: ExecutionContext) => {
-  const [, , { req }] = context.getArgs();
-  const userData = getClaims(req.headers.authorization);
+  const [, , { headers }] = context.getArgs();
+  const userData = getClaims(headers.authorization);
   const email = userData.email || userData.upn;
   return email;
 });
--- EdFi.Buzz.Api/src/graphql/guards/validateStaffId.guard.ts
+++ EdFi.Buzz.Api/src/graphql/guards/validateStaffId.guard.ts
@@ -12,12 +12,12 @@ import getClaims from '../services/jwt.service';
 @Injectable()
 export default class ValidateStaffIdGuard implements CanActivate {
   // eslint-disable-next-line no-useless-constructor
-  constructor(private readonly staffService: StaffService) {}
+  constructor(private readonly staffService: StaffService) { }
 
   // eslint-disable-next-line class-methods-use-this
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const [, params, { req }] = context.getArgs();
-    const userData = getClaims(req.headers.authorization);
+    const [, params, { headers }] = context.getArgs();
+    const userData = getClaims(headers.authorization);
     const errorMessage = 'You don\'t have access to execute this query or the parameters are not valid.';
 
     if (!userData) {
