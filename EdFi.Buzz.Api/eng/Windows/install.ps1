7a42f85f502b9ddb2cc482b4e5ddc6d669b52133
Kurt Johnson <kurt.johnson@headspring.com>
2020-09-29 15:08:44 -0500

    Update the API with latest install script
    Adds app common just like API.

--- EdFi.Buzz.Api/eng/Windows/install.ps1
+++ EdFi.Buzz.Api/eng/Windows/install.ps1
@@ -9,7 +9,7 @@
 [CmdletBinding()]
 param(
   [string]
-  $InstallPath = "C:\inetpub\Ed-Fi\Buzz\API",
+  $InstallPath = "C:\Ed-Fi\Buzz",
 
   [string]
   $NginxUrl = "http://nginx.org/download/nginx-1.19.0.zip",
@@ -34,11 +34,22 @@ param(
   $DbName = "Buzz",
 
   [int]
-  $HttpPort = 3000
+  $port = 3000,
+  [Parameter(Mandatory = $true)]
+  [string] $toolsPath,
+  [Parameter(Mandatory = $true)]
+  [string] $packagesPath,
+  [Parameter(Mandatory = $true)]
+  [string] $nginxPort,
+  [Parameter(Mandatory = $true)]
+  [string] $rootDir = "dist",
+  [Parameter(Mandatory = $true)]
+  [string] $app = "UI"
 )
 
-# TODO: refactor to share functions instead of duplicate them
-# in various application installer scripts.
+Import-Module "$PSScriptRoot/init.psm1" -Force
+Initialize-Installer -toolsPath $toolsPath  -packagesPath $packagesPath
+
 function Get-FileNameWithoutExtensionFromUrl {
   param(
     [string]
@@ -55,29 +66,30 @@ function Get-FileNameWithoutExtensionFromUrl {
 function Get-HelperAppIfNotExists {
   param(
     [string]
-    $Url
+    $Url,
+    [string]
+    $targetLocation
   )
   $version = Get-FileNameWithoutExtensionFromUrl -Url $Url
 
-  if (-not (Test-Path -Path "$InstallPath\$version")) {
+  if (-not (Test-Path -Path "$targetLocation\$version")) {
     $outFile = ".\$version.zip"
     Invoke-WebRequest -Uri $Url -OutFile $outFile
 
     Expand-Archive -Path $outFile
 
     if ((Test-Path -Path "$version\$version")) {
-      Move-Item -Path "$version\$version" -Destination $InstallPath
+      Move-Item -Path "$version\$version" -Destination $targetLocation
       Remove-Item -Path $version
     }
     else {
-      Move-Item -Path "$version" -Destination $InstallPath
+      Move-Item -Path "$version" -Destination $targetLocation
     }
   }
 
   return $version
 }
-
-function Install-NginxFiles{
+function Install-NginxFiles {
   param(
     [string]
     $nginxVersion,
@@ -87,18 +99,20 @@ function Install-NginxFiles{
 
   # Copy the build directory into the NGiNX folder
   $parameters = @{
-    Path = "$webSitePath\build"
-    Destination = "$webSitePath\$nginxVersion\build"
-    Recurse = $true
-    Force = $true
+    Path        = "$webSitePath\$rootDir"
+    Destination = "$webSitePath\$nginxVersion\$rootDir"
+    Recurse     = $true
+    Force       = $true
   }
   Copy-Item @parameters
 
+  Update-NginxConf -installPath $iisParams.WebApplicationPath
+
   # Overwrite the NGiNX conf file with our conf file
   $paramaters = @{
-    Path = "$webSitePath\nginx.conf"
+    Path        = "$webSitePath\nginx.conf"
     Destination = "$webSitePath\$nginxVersion\conf\nginx.conf"
-    Force = $true
+    Force       = $true
   }
   Copy-Item @paramaters
 }
@@ -106,10 +120,15 @@ function Install-NginxFiles{
 function Install-NginxService {
   param(
     [string]
-    $winSwVersion
+    $nginxVersion,
+
+    [string]
+    $winSwVersion,
+    [string]
+    $webSitePath
   )
 
-  $serviceName = "EdFi-Buzz-Api"
+  $serviceName = "EdFi-Buzz-$script:app"
 
   # If service already exists, stop and remove it so that new settings
   # will be applied.
@@ -120,25 +139,25 @@ function Install-NginxService {
     &sc.exe delete $serviceName
   }
 
-  # Rename WindowsService.exe to BuzzApi.exe
-  $winServiceExe = "$InstallPath\$winSwVersion\WindowsService.exe"
-  $edFiBuzzApiExe = "$InstallPath\$winSwVersion\EdFiBuzzApi.exe"
-  if ((Test-Path -Path "$InstallPath\$winSwVersion\WindowsService.exe")) {
-    Move-Item -Path $winServiceExe -Destination $edFiBuzzApiExe
+  # Rename WindowsService.exe to EdFiBuzzUi.exe
+  $winServiceExe = "$webSitePath\$winSwVersion\WindowsService.exe"
+  $edFiBuzzExe = "$webSitePath\$winSwVersion\EdFiBuzz$($script:app).exe"
+  if ((Test-Path -Path "$webSitePath\$winSwVersion\WindowsService.exe")) {
+    Move-Item -Path $winServiceExe -Destination $edFiBuzzExe
   }
 
   # Copy the config XML file to install directory
-  $xmlFile = "$InstallPath\$winSwVersion\EdFiBuzzApi.xml"
-  Copy-Item -Path "$PSScriptRoot\EdFiBuzzApi.xml" -Destination $xmlFile -Force
+  $xmlFile = "$webSitePath\$winSwVersion\EdFiBuzz$($script:app).xml"
+  Copy-Item -Path "$PSScriptRoot\EdFiBuzz$($script:app).xml" -Destination $xmlFile -Force
 
   # Inject the correct path to nginx.exe into the config XML file
   $content = Get-Content -Path $xmlFile -Encoding UTF8
-  $content = $content.Replace("{0}", "$InstallPath")
+  $content = $content.Replace("{0}", "$webSitePath\$nginxVersion")
   $content | Out-File -FilePath $xmlFile -Encoding UTF8 -Force
 
   # Create and start the service
-  &$edFiBuzzApiExe install
-  &$edFiBuzzApiExe start
+  &$edFiBuzzExe install
+  &$edFiBuzzExe start
 }
 
 function New-DotEnvFile {
@@ -156,11 +175,38 @@ BUZZ_API_DB_PORT = $DbPort
 BUZZ_API_DB_USERNAME ='$DbUserName'
 BUZZ_API_DB_PASSWORD = '$DbPassword'
 BUZZ_API_DB_DATABASE = '$DbName'
-BUZZ_API_HTTP_PORT = $HttpPort
+BUZZ_API_HTTP_PORT = $port
 "@
 
   $fileContents | Out-File "$InstallPath\.env" -Encoding UTF8 -Force
 }
+
+function Update-WebConfig {
+  param(
+    [string]
+    $installPath,
+    [string]
+    $nginxPort
+  )
+
+  $fileContents = (Get-Content -Path "$installPath/web.config" -Encoding UTF8).Replace("%NGINXPORT%", $script:nginxPort)
+  $fileContents | Out-File "$installPath/web.config" -Encoding UTF8 -Force
+}
+
+function Update-NginxConf {
+  param(
+    [string]
+    $installPath
+  )
+
+  $fileContents = (Get-Content -Path "$installPath/nginx.conf" -Encoding UTF8)
+  $fileContents = $fileContents.Replace("%NGINXPORT%", $script:nginxPort)
+  $fileContents = $fileContents.Replace("%WEBROOT%", "./$script:rootDir")
+  $fileContents = $fileContents.Replace("`r`n", "`n")
+  $nginxFile = Resolve-Path("$installPath/nginx.conf")
+  [IO.File]::WriteAllText($nginxFile, $fileContents) # prevents final CR-LF
+}
+
 
 function Install-DistFiles {
   param(
@@ -168,9 +214,9 @@ function Install-DistFiles {
     $installPath
   )
 
-  # Copy the dist directory into the NGiNX folder
+  # Copy the build directory into the NGiNX folder
   $parameters = @{
-    Path        = "$PSScriptRoot/../dist"
+    Path        = "$PSScriptRoot/../$rootDir"
     Destination = "$installPath"
     Recurse     = $true
     Force       = $true
@@ -178,27 +224,30 @@ function Install-DistFiles {
   Copy-Item @parameters
 }
 
-Write-Host "Begin Ed-Fi Buzz API installation..." -ForegroundColor Yellow
-
-New-Item -Path $InstallPath -ItemType Directory -Force | Out-Null
-
-Install-DistFiles -installPath $InstallPath
-New-DotEnvFile -installPath  "$InstallPath/dist"
+Write-Host "Begin Ed-Fi Buzz $($script:app) installation..." -ForegroundColor Yellow
 
 $iisParams = @{
-  SourceLocation = "$PSScriptRoot\API"
+  SourceLocation = "$PSScriptRoot\.."
   WebApplicationPath = "C:\inetpub\Ed-Fi\Buzz\API"
   WebApplicationName = "BuzzAPI"
   WebSitePort = $configuration.ui.port
   WebSiteName = "Ed-Fi-Buzz-API"
 }
 
-# Install-EdFiApplicationIntoIIS @iisParams
+New-Item -Path $iisParams.WebApplicationPath -ItemType Directory -Force | Out-Null
 
-$nginxVersion = Get-HelperAppIfNotExists -Url $NginxUrl
-Install-NginxFiles -nginxVersion $nginxVersion
+Install-DistFiles -installPath $InstallPath
+New-DotEnvFile -installPath  "$InstallPath/dist"
 
-$winSwVersion = Get-HelperAppIfNotExists -Url $WinSWUrl
-Install-NginxService -winSwVersion $winSwVersion
+Install-EdFiApplicationIntoIIS @iisParams
 
-Write-Host "End Ed-Fi Buzz API installation." -ForegroundColor Yellow
+$nginxVersion = Get-HelperAppIfNotExists -Url $NginxUrl -targetLocation $iisParams.WebApplicationPath
+Install-NginxFiles -nginxVersion $nginxVersion -webSitePath $iisParams.WebApplicationPath
+
+Update-WebConfig -installPath $iisParams.WebApplicationPath
+
+# NGINX will be mapped to a different port and redirect thru ARR Reverse Proxy in the web.config.
+$winSwVersion = Get-HelperAppIfNotExists -Url $WinSWUrl -targetLocation $iisParams.WebApplicationPath
+Install-NginxService -nginxVersion $nginxVersion -winSwVersion $winSwVersion -webSitePath $iisParams.WebApplicationPath
+
+Write-Host "End Ed-Fi Buzz $($script:app) installation." -ForegroundColor Yellow
