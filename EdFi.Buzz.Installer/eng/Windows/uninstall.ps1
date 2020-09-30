983f7d3d8b895fb1677091a2232ccb754154c910
Kurt Johnson <kurt.johnson@headspring.com>
2020-09-30 09:25:44 -0500

    Force continue on web uninstall

--- EdFi.Buzz.Installer/eng/Windows/uninstall.ps1
+++ EdFi.Buzz.Installer/eng/Windows/uninstall.ps1
@@ -60,8 +60,8 @@ try {
     Initialize-Installer -toolsPath $toolsPath  -packagesPath $packagesPath
 
     Uninstall-BuzzApp -app "ETL" -appPath  (Join-Path $installPath "ETL")
-    Uninstall-BuzzApp -app "API" -appPath  (Join-Path $installPath "API")
-    Uninstall-BuzzApp -app "UI" -appPath  (Join-Path $installPath "UI")
+    Uninstall-BuzzApp -app "API" -appPath  "C:/inetpub/Ed-Fi/Buzz/API"
+    Uninstall-BuzzApp -app "UI" -appPath  "C:/inetpub/Ed-Fi/Buzz/UI"
 }
 catch {
     Write-Error $PSItem.Exception.Message
