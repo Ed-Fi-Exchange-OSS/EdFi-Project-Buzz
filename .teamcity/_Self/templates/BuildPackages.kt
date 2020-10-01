bc24c97f61b014219d96882d189519a1d6a35299
Kurt Johnson <kurt.johnson@headspring.com>
2020-10-01 13:47:59 -0500

    Add missing VcsTrigger import

--- .teamcity/_Self/templates/BuildPackages.kt
+++ .teamcity/_Self/templates/BuildPackages.kt
@@ -9,6 +9,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.freeDiskSpace
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.swabra
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.powerShell
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.VcsTrigger
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
 
