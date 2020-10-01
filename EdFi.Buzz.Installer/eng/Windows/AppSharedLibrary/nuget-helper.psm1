f80270d039a4b1d92c9be090b753e3288e550a6b
DavidJGapCR <djimenez@growthaccelerationpartners.com>
2020-10-01 16:04:52 -0600

    Working on details.

--- EdFi.Buzz.Installer/eng/Windows/AppSharedLibrary/nuget-helper.psm1
+++ EdFi.Buzz.Installer/eng/Windows/AppSharedLibrary/nuget-helper.psm1
@@ -14,7 +14,7 @@ function Install-EdFiPackage {
 
     $downloadedPackagePath = Join-Path $downloadPath "$packageName.$version"
 
-    &"$toolsPath\nuget" install $packageName -source $packageSource -Version $version -outputDirectory $downloadPath -ConfigFile "$PSScriptRoot\nuget.config" | Out-Host
+    &"$toolsPath\nuget" install $packageName -source $packageSource -Version $version -outputDirectory $downloadPath -ConfigFile "$PSScriptRoot\..\nuget.config" | Out-Host
 
     if ($LASTEXITCODE) {
         throw "Failed to install package $packageName $version"
