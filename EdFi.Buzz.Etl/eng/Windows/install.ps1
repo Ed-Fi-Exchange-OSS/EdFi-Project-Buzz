88b945a6b21e002af108d299a0c80734ad15dad8
Kurt Johnson <kurt.johnson@headspring.com>
2020-11-11 15:13:05 -0600

    Escape ETL strings in winsw file
    We need to ensure that the XML encoding is valid.

--- EdFi.Buzz.Etl/eng/Windows/install.ps1
+++ EdFi.Buzz.Etl/eng/Windows/install.ps1
@@ -97,11 +97,11 @@ function Install-NodeService {
   # Inject the correct path to nginx.exe into the config XML file
   $content = Get-Content -Path $xmlFile -Encoding UTF8
   $content = $content.Replace("{0}", "$InstallPath")
-  $content = $content.Replace("{1}", "$PostgresUserName")
-  $content = $content.Replace("{2}", "$PostgresPassword")
-  $content = $content.Replace("{3}", "$PostgresHost")
-  $content = $content.Replace("{4}", "$PostgresPort")
-  $content = $content.Replace("{5}", "$PostgresDbName")
+  $content = $content.Replace("{1}", [uri]::EscapeUriString("$PostgresUserName"))
+  $content = $content.Replace("{2}", [uri]::EscapeUriString("$PostgresPassword"))
+  $content = $content.Replace("{3}", [uri]::EscapeUriString("$PostgresHost"))
+  $content = $content.Replace("{4}", [uri]::EscapeUriString("$PostgresPort"))
+  $content = $content.Replace("{5}", [uri]::EscapeUriString("$PostgresDbName"))
   $content = $content.Replace("{6}", "$InstallPath/dist")
   $content | Out-File -FilePath $xmlFile -Encoding UTF8 -Force
 
