a3eecd2609b8665a8106042c901f6f00ad9358f1
Kurt Johnson <kurt.johnson@headspring.com>
2020-11-17 17:14:24 -0600

    Specify CORS headers for API
    Added fastify request logging to test problems with CORS

--- EdFi.Buzz.Api/src/main.ts
+++ EdFi.Buzz.Api/src/main.ts
@@ -4,6 +4,7 @@
 // See the LICENSE and NOTICES files in the project root for more information.
 
 import { Logger } from '@nestjs/common';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import AppModule from './app.module';
@@ -11,7 +12,7 @@ import AppModule from './app.module';
 const httpPort: number = parseInt(process.env.BUZZ_API_HTTP_PORT, 10);
 const allowedOrigins: string[] = process.env.BUZZ_API_CORS_ORIGINS.split(',');
 
-const corsOptions = {
+const corsOptions: CorsOptions = {
   origin: (origin, callback) => {
     if (allowedOrigins.includes(origin) || !origin) {
       callback(null, true);
@@ -19,10 +20,23 @@ const corsOptions = {
       callback(new Error(`Origin ${origin} was not defined in BUZZ_API_CORS_ORIGINS`));
     }
   },
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: [
+    'Authorization',
+    'Access-Control-Allow-Origin',
+    'Access-Control-Allow-Headers',
+    'Origin',
+    'Accept',
+    'X-Requested-With',
+    'Content-Type',
+    'Access-Control-Request-Method',
+    'Access-Control-Request-Headers'
+  ],
+  credentials: true,
 };
 
 async function bootstrap() {
-  const fastifyAdapter = new FastifyAdapter({ trustProxy: true });
+  const fastifyAdapter = new FastifyAdapter({ trustProxy: true, logger: true });
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, fastifyAdapter);
   app.enableCors({ ...corsOptions });
   Logger.log(`NODE_TLS_REJECT_UNAUTHORIZED := ${process.env.NODE_TLS_REJECT_UNAUTHORIZED}`);
