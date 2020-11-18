6a38ddc42090caefe8f5bb8886c2065649672419
José Alberto Leiva Obando <56046999+jleiva-gap@users.noreply.github.com>
2020-11-18 15:20:45 -0600

    [FIF-351] Added Attendance table to Buzz database (#162)

---
+++ EdFi.Buzz.Database/migrations/sqls/20201117102215-create-attendance-tables-up.sql
@@ -0,0 +1,15 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+CREATE TABLE buzz.Attendance (
+    StudentSchoolKey VARCHAR(45) NOT NULL,
+    ReportedAsPresentAtSchool NUMERIC(5,2) NOT NULL DEFAULT 0,
+    ReportedAsAbsentFromSchool NUMERIC(5,2) NOT NULL DEFAULT 0,
+    ReportedAsPresentAtHomeRoom NUMERIC(5,2) NOT NULL DEFAULT 0,
+    ReportedAsAbsentFromHomeRoom NUMERIC(5,2) NOT NULL DEFAULT 0,
+    ReportedAsIsPresentInAllSections NUMERIC(5,2) NOT NULL DEFAULT 0,
+    ReportedAsAbsentFromAnySection NUMERIC(5,2) NOT NULL  DEFAULT 0,
+    CONSTRAINT PK_AttendanceKey PRIMARY KEY (StudentSchoolKey)
+);
