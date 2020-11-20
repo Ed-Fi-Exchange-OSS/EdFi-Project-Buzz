2cf763d8035819c13fc698c69e8a3b10729beb39
David Jimenez Barrantes <djimenez@growthaccelerationpartners.com>
2020-11-20 13:36:01 -0600

    [FIF-352] Includes chronic absenteeism data from AMT in the ETL scripts. (#163)

---
+++ EdFi.Buzz.Etl/sql/amt/0009-ImportChronicAbsenteeismData.sql
@@ -0,0 +1,16 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+SELECT
+    studentschoolkey,
+    cast(sum(ReportedAsPresentAtSchool) * 1.0 / count(DateKey) as numeric(5,2)) as reportedaspresentatschool,
+    cast(sum(ReportedAsAbsentFromSchool) * 1.0 / count(DateKey) as numeric(5,2)) as reportedasabsentfromschool,
+    cast(sum(ReportedAsAbsentFromSchoolReportedAsPresentAtHomeRoom) * 1.0 / count(DateKey) as numeric(5,2)) as reportedaspresentathomeroom,
+    cast(sum(ReportedAsAbsentFromHomeRoom) * 1.0 / count(DateKey) as numeric(5,2)) as reportedasabsentfromhomeroom,
+    cast(sum(ReportedAsIsPresentInAllSections) * 1.0 / count(DateKey) as numeric(5,2)) as reportedasispresentinallsections,
+    cast(sum(ReportedAsAbsentFromAnySection) * 1.0 / count(DateKey) as numeric(5,2)) as reportedasabsentfromanysection
+FROM analytics.chrab_ChronicAbsenteeismAttendanceFact
+GROUP BY StudentSchoolKey
+ORDER BY StudentSchoolKey;
--- EdFi.Buzz.Etl/src/dbETL.js
+++ EdFi.Buzz.Etl/src/dbETL.js
@@ -11,6 +11,7 @@ const config = require('../config/dbs');
 const etl = require('../config/etl');
 const loadMsSqlData = require('./loadMsSqlData');
 const odsSurveyProcessor = require('../processors/odsSurveyProcessor');
+const dsHelper = require('../config/dsHelper');
 
 const pg = config.pgConfig;
 const ms = config.mssqlConfig;
@@ -29,6 +30,16 @@ const loadBaseEntities = async () => {
     await loadMsSqlData.loadMsSqlData(pg, ms, etl.staffSectionConfig());
     await loadMsSqlData.loadMsSqlData(pg, ms, etl.studentSectionConfig);
     await loadMsSqlData.loadMsSqlData(pg, ms, etl.studentContactConfig);
+
+    if (process.env.BUZZ_SQLSOURCE === 'amt') {
+      const viewExists = await dsHelper.chronicAbsenteeismAttendanceFactExists(ms);
+      if (viewExists === 'yes') {
+        await loadMsSqlData.loadMsSqlData(pg, ms, etl.studentAttendanceConfig);
+      } else {
+        console.log('chrab_ChronicAbsenteeismAttendanceFact is not installed');
+      }
+    }
+
     console.log('finished loading entities');
 
     if (process.env.KeepSurveysSynch && process.env.KeepSurveysSynch.toLowerCase() === 'true') {
