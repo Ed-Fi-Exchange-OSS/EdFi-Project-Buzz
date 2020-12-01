8c3bc9378b3c9288945477ab120f6ecb52332604
Kurt Johnson <kurt.johnson@headspring.com>
2020-12-01 16:49:26 -0600

    Add assessment sample data

---
+++ EdFi.Buzz.Database/migrations/sqls/20201201170923-alter-studentasssessment-up.sql
@@ -0,0 +1,18 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+ALTER TABLE  buzz.studentassessment
+	ADD studentassessmentkey VARCHAR(45) NOT NULL DEFAULT gen_random_uuid ();
+
+ALTER TABLE  buzz.studentassessment
+	DROP CONSTRAINT PK_StudentAssessmentKey;
+
+ALTER TABLE  buzz.studentassessment
+  ADD CONSTRAINT PK_StudentAssessmentKey
+    PRIMARY KEY (studentassessmentkey);
+
+ALTER TABLE  buzz.studentassessment
+	ADD CONSTRAINT studentschoolkey_assessmentidentifier_ux UNIQUE (studentschoolkey, assessmentidentifier);
+
