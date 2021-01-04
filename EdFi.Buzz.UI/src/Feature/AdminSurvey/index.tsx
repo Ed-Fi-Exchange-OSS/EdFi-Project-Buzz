50b61b63308072aa5ebc1dd0e17e55408d276b0d
Kurt Johnson <kurt.johnson@headspring.com>
2021-01-04 09:24:41 -0600

    Fix admin survey load screen bug
    The appMounted variable needs to be set outside the scope of Hooks to prevent loop.

--- EdFi.Buzz.UI/src/Feature/AdminSurvey/index.tsx
+++ EdFi.Buzz.UI/src/Feature/AdminSurvey/index.tsx
@@ -151,6 +151,7 @@ const StyledTextParent = styled.div`
 export const AdminSurvey: FunctionComponent<AdminSurveyComponentProps> = (props: AdminSurveyComponentProps) => {
   document.title = 'EdFi Buzz: Admin Surveys';
 
+  const [appMounted, SetAppMounted] = useState(false);
   const [surveyFilteredList, setsurveyFilteredList] = useState([] as SurveyStatus[]);
   const [surveyList, setsurveyList] = useState([] as SurveyStatus[]);
   const [surveyToDelete, setsurveyToDelete] = useState(null as number);
@@ -159,7 +160,7 @@ export const AdminSurvey: FunctionComponent<AdminSurveyComponentProps> = (props:
   const surveyFilterRef = React.createRef<HTMLInputElement>();
 
   useEffect(() => {
-    let appMounted=true;
+    SetAppMounted(true);
     if(appMounted){
       if (!surveyFilteredList || surveyFilteredList.length === 0) {
         props.api.survey.getSurveyStatus(
@@ -171,7 +172,7 @@ export const AdminSurvey: FunctionComponent<AdminSurveyComponentProps> = (props:
       }
     }
     return () => {
-      appMounted=false;
+      SetAppMounted(false);
     };
   }, [props.api.authentication.currentUserValue.teacher.staffkey,
     props.api.survey,
